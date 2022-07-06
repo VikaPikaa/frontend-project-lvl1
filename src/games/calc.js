069d884a906ab9e3a6e8d634016999cf824edee3
Vika Pika <victoriamalich11@gmail.com>
2022-07-06 22:59:01 +0400

    changes

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-eval */
 import { randomNum, randomOperator } from '../math.js';
 
 const calc = () => {
   const question = 'What is the result of the expression?';
   const target = `${randomNum()} ${randomOperator()} ${randomNum()}`;
+  // eslint-disable-next-line no-eval
   const correctAnswer = `${eval(target)}`;
   return [question, target, correctAnswer];
 };
