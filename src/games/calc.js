31fe1b4fdb03b2d21f39e93a000735b52f2efe6b
Vika Pika <victoriamalich11@gmail.com>
2022-07-06 23:01:21 +0400

    remove eval

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,8 +3,7 @@ import { randomNum, randomOperator } from '../math.js';
 const calc = () => {
   const question = 'What is the result of the expression?';
   const target = `${randomNum()} ${randomOperator()} ${randomNum()}`;
-  // eslint-disable-next-line no-eval
-  const correctAnswer = `${eval(target)}`;
+  const correctAnswer = `${(target)}`;
   return [question, target, correctAnswer];
 };
 export default calc;
